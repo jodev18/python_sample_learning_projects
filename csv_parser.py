21404f1e1ebe7d0866835363a13e6e38592253cd
user <josiahbuhayan@gmail.com>
2018-10-17 15:20:22 +0800

    added sections

--- csv_parser.py
+++ csv_parser.py
@@ -7,20 +7,25 @@ with open('evaluation_resp.csv') as csvfile:
 
     cleaned_names = []
     sections = []
+    arrivals = []
+
     for row in reader:
         rec_timestamp = row['Timestamp']
         teacher = row['Name of the Teacher'].split()
         section = row['Grade/Year & Section']
+        arrival = row['Arrives and Dismisses class on time.']
 
         #process names
 
         if len(teacher) > 1:
             if teacher[0] not in undesired_names:
+               teacher[0] = teacher[0].capitalize()
                cleaned_names.append(' '.join(teacher[0:]))
         else:
             print("Single name: ",teacher)
 
         sections.append(section)
+        arrivals.append(arrival)
 
         print(row['Timestamp'],row['Name of the Teacher'].split())
         print(row)
