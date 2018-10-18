cd73e4e509ba6d3ce38e6d41d914c61586348279
jojo <josiahbuhayan@gmail.com>
2018-10-18 11:14:51 +0800

    updating manipulation of fields for csv parser

--- csv_parser.py
+++ csv_parser.py
@@ -1,6 +1,7 @@
 import csv
 
-undesired_names = ['Maam','maam','Ma.','sir','Sir','SIR','Mrs.','ma.','ma','Dr.','dr','DR','mr.','Mr.']
+undesired_names = ['Maam','maam','Ma.','sir','Sir','SIR','Mrs.','ma.','ma','Dr.',
+                   'dr','DR','mr.','Mr.','Ms.','Sr.','Sir.','T.','Teacher']
 column_keys = []
 indexed_keys = []
 
@@ -19,8 +20,15 @@ with open('evaluation_resp.csv') as csvfile:
     clear_instructions = []
     mastery_of_subject = []
     explains_lesson_clearly = []
+    equal_treatment = []
+    emphasizes_relevance = []
+    using_visual_aids = []
+    gives_tests = []
+    personality_fair = []
+    comments = []
 
-
+    row_count = sum(1 for row in csvfile)
+    print("Total row length",row_count)
 
     for row in reader:
         column_keys = row.keys()
@@ -33,13 +41,36 @@ with open('evaluation_resp.csv') as csvfile:
         organized = row[indexed_keys[4]]
         climate_order_mngt = row[indexed_keys[5]]
         cleanliness_check = row[indexed_keys[6]]
+        time_efficiency_d = row[indexed_keys[7]]
+        english_elip_d = row[indexed_keys[8]]
+        vld_d = row[indexed_keys[9]]
+        clear_instruc = row[indexed_keys[10]]
+        subj_mastery = row[indexed_keys[11]]
+        clear_explain = row[indexed_keys[12]]
+        eq_treat = row[indexed_keys[13]]
+        emp_relevance = row[indexed_keys[14]]
+        vis_aid = row[indexed_keys[15]]
+        test_prov = row[indexed_keys[16]]
+        fair_person = row[indexed_keys[17]]
+        comment = row[indexed_keys[18]]
 
         #process names
 
         if len(teacher) > 1:
+
+            proc_name = []
+
+            for name in teacher:
+                proc_name.append(name.capitalize())
+
+            print("processed: ", proc_name)
+
             if teacher[0] not in undesired_names:
                teacher[0] = teacher[0].capitalize()
-               cleaned_names.append(' '.join(teacher[0:]))
+               cleaned_names.append(' '.join(proc_name[0:]))
+            else:
+                teacher[1] = teacher[1].capitalize()
+                cleaned_names.append(' '.join(proc_name[1:]))
         else:
             print("Single name: ",teacher)
 
@@ -49,9 +80,6 @@ with open('evaluation_resp.csv') as csvfile:
         manage_climate_orders.append(climate_order_mngt)
         cleanliness_checks.append(cleanliness_check)
 
-        print(row['Timestamp'],row['Name of the Teacher'].split())
-        print(row)
-
     print(cleaned_names)
 
     for i in range(len(cleaned_names)):
@@ -60,5 +88,7 @@ with open('evaluation_resp.csv') as csvfile:
     print(indexed_keys)
     print("Number of columns processed: ",len(indexed_keys))
 
+    cleaned_names.sort()
+    print(cleaned_names)
 
 
